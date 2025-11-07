c3a06ec05780bd307aea2676cb97ddfa5696145e
Aluno <Aluno@exemplo.com>
2025-11-07 16:34:01 -0300

    Adicionando novos arquivos na aula16

---
+++ aula16/pooClasses/classeRetangulo/scripts.js
@@ -0,0 +1,40 @@
+class Retangulo{
+    constructor(nome, largura, altura) {
+        this.nome = nome;
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    obterNome() {
+        valorNome = prompt('Digite o nome do retãngulo');
+    }
+
+    obterLargura() {
+        valorLargura = Number(prompt('Digite a largura'));
+    }
+
+    obterArea() {
+        valorAltura = Number(prompt('Digite a altura'));
+    }
+
+    calcularPerimetro() {
+
+    }
+
+    calcularArea() {
+
+    }
+
+    atualizarLargura(novaLargura) {
+
+    }
+
+    atualizarAltura(novaAltura) {
+
+    }
+}
+
+
+
+
+
