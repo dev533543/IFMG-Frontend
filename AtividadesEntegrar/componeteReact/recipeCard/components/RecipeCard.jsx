b8a47b6be425aeb9773d0c9279bad9f1a05bc17c
Marcus1423 <marquinhus.vinicius10@gmail.com>
2025-11-09 21:30:53 -0300

    Finalizando as atividades

--- AtividadesEntegrar/componeteReact/recipeCard/components/RecipeCard.jsx
+++ AtividadesEntegrar/componeteReact/recipeCard/components/RecipeCard.jsx
@@ -1,7 +1,7 @@
 function RecipeCard() {
     const receita = {
         titulo: "Bolo de Chocolate",
-        tempoPreparo: 90,
+        tempoPreparo: 50,
         dificuldade: "Médio",
         ingredientes: [
             "2 xícaras de farinha de trigo",
@@ -14,12 +14,34 @@ function RecipeCard() {
         modoPreparo: "Misture todos os ingredientes, coloque em uma forma untada e leve ao forno por 40 minutos a 180°C."
     }
 
+    if (receita.tempoPreparo < 60) {
+        receita.dificuldade = "Fácil";
+    } else if (receita.tempoPreparo >= 60 && receita.tempoPreparo < 90) {
+        receita.dificuldade = "Médio";
+    } else if (receita.tempoPreparo > 90) {
+        receita.dificuldade = "Difícil";
+    }
+
+    
+
+    const corDificuldade = {
+        Fácil: "green",
+        Médio: "orange",
+        Difícil: "red"
+    };
+
+    const estiloDificuldade = {
+        color: corDificuldade[receita.dificuldade],
+    };
+
     return (
         <>
             <div className="recipeCard">
                 <h2>{receita.titulo}</h2>
 
-                <p><strong>Dificuldade:</strong> {receita.dificuldade}</p>
+                <p><strong>tempo de preparo:</strong> {receita.tempoPreparo} minutos.</p>
+
+                <p style={estiloDificuldade}><strong>Dificuldade:</strong> {receita.dificuldade}.</p>
 
                 <ul>
                     {receita.ingredientes.map((item, index) => (
